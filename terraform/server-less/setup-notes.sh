aea767a04c1c7b7bd9af7023d0eda68bea62559a
PengHian Ang <v_fphhang@tencent.com>
2023-06-30 21:57:39 +0800

    finished server-based and server-less resume-website 2023-06-30

--- terraform/server-less/setup-notes.sh
+++ terraform/server-less/setup-notes.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+
+# run this after terraform apply is finish to enable kubectl
 aws eks --region $(terraform output -raw region) update-kubeconfig \
     --name $(terraform output -raw cluster_name)
 
+# copy this into end of jenkins build as execute shell
 cp -r /var/jenkins_home/workspace/resume-website/website/* /usr/share/nginx/html/
